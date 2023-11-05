c47f9123b98758a5632dcad1c8e31f9de5a620d1
solinad21 <apolonioluis@hotmail.com>
2023-11-05 13:46:23 -0700

    commit fix ports problems

--- apps/gateway2/src/main.ts
+++ apps/gateway2/src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3008);
+  await app.listen(3000);
   console.log(`Gateway is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+
